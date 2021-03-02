31d13c24d5e17703b871432e4eb2369affb099f6
cleberpinelli <pinellicpt@fab.mil.br>
2021-03-02 15:07:11 -0300

    Criando projeto com spring initializr

---
+++ src/main/java/br/uniriotec/sal/ib/InformationBehaviorApplication.java
@@ -0,0 +1,13 @@
+package br.uniriotec.sal.ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationBehaviorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationBehaviorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uniriotec/sal/ib/InformationBehaviorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uniriotec.sal.ib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationBehaviorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
